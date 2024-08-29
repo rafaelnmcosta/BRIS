d3f85abf7352860f330088df947c5ceb37a86de9
iuryalvez <iuryalves774@gmail.com>
2024-08-28 22:21:49 -0300

    homes

--- front-end/src/components/ProtectedRoute.js
+++ front-end/src/components/ProtectedRoute.js
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 // Função para verificar se o usuário está autenticado
 const isAuthenticated = () => {
     const token = localStorage.getItem('jwtToken');
-    return !!token;
+    return true;
 };
 
 // Componente de rota protegida
